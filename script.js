80b0ad7710d14f58e74f9c1243dbff54b0b8e6a3
Jax I <jaxpi85@gmail.com>
2023-02-08 12:37:48 -0500

    added new game start function

--- script.js
+++ script.js
@@ -387,4 +387,118 @@ window.onload = function () {
     }
   }
 
+ // START NEW GAME
+ function newGame() {
+  score = 0;
+  gamestate = gamestates.ready;
+  gameover = false;
+
+  createLevel();
+  findMoves();
+  findClusters();
+}
+
+function createLevel() {
+  var done = false;
+
+  while (!done) {
+    for (var i = 0; i < level.columns; i++) {
+      for (var j = 0; j < level.rows; j++) {
+        level.tiles[i][j].type = getRandomTile();
+      }
+    }
+
+    resolveClusters();
+    findMoves();
+
+    if (moves.length > 0) {
+      done = true;
+    }
+  }
+}
+
+function getRandomTile() {
+  return Math.floor(Math.random() * tilecolors.length);
+}
+
+function resolveClusters() {
+  findClusters();
+
+  while (clusters.length > 0) {
+    removeClusters();
+    shiftTiles();
+    findClusters();
+  }
+}
+
+function findClusters() {
+  clusters = [];
+
+  for (var j = 0; j < level.rows; j++) {
+    var matchlength = 1;
+    for (var i = 0; i < level.columns; i++) {
+      var checkcluster = false;
+
+      if (i == level.columns - 1) {
+        checkcluster = true;
+      } else {
+        if (
+          level.tiles[i][j].type == level.tiles[i + 1][j].type &&
+          level.tiles[i][j].type != -1
+        ) {
+          matchlength += 1;
+        } else {
+          checkcluster = true;
+        }
+      }
+
+      if (checkcluster) {
+        if (matchlength >= 3) {
+          clusters.push({
+            column: i + 1 - matchlength,
+            row: j,
+            length: matchlength,
+            horizontal: true,
+          });
+        }
+
+        matchlength = 1;
+      }
+    }
+  }
+
+  for (var i = 0; i < level.columns; i++) {
+    var matchlength = 1;
+    for (var j = 0; j < level.rows; j++) {
+      var checkcluster = false;
+
+      if (j == level.rows - 1) {
+        checkcluster = true;
+      } else {
+        if (
+          level.tiles[i][j].type == level.tiles[i][j + 1].type &&
+          level.tiles[i][j].type != -1
+        ) {
+          matchlength += 1;
+        } else {
+          checkcluster = true;
+        }
+      }
+
+      if (checkcluster) {
+        if (matchlength >= 3) {
+          clusters.push({
+            column: i,
+            row: j + 1 - matchlength,
+            length: matchlength,
+            horizontal: false,
+          });
+        }
+
+        matchlength = 1;
+      }
+    }
+  }
+}
+
 };
